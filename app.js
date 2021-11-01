491d7e41cbd67fc6d7f0bae773f0d2fe8ee8f436
virajrajopadhye <virajrajopadhye@gmail.com>
2021-10-31 22:15:49 -0500

    enabling statuc content

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.use(
